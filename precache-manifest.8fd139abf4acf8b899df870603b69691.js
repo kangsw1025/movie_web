ff57c98d7e2325dc0145dc2581a527e7c8b3398b
9ang <kangsw1025@naver.com>
2020-10-18 22:54:40 +0900

    Updates

---
+++ precache-manifest.8fd139abf4acf8b899df870603b69691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0455c89bef838958eefc909ce99b9d8",
+    "url": "/movie_web/index.html"
+  },
+  {
+    "revision": "47b257bbdd5c4e88f8bf",
+    "url": "/movie_web/static/css/main.25d3ce76.chunk.css"
+  },
+  {
+    "revision": "95fea87aafe90798f822",
+    "url": "/movie_web/static/js/2.4e2e396d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_web/static/js/2.4e2e396d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b257bbdd5c4e88f8bf",
+    "url": "/movie_web/static/js/main.81fd4e61.chunk.js"
+  },
+  {
+    "revision": "e7341a00153fc981e5cc",
+    "url": "/movie_web/static/js/runtime-main.ca7361f0.js"
+  }
+]);
